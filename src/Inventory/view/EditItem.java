1bbe4af78dc401bc720633fba5fd09ee5e47012d
unknown <icebecool3me@yahoo.com>
2013-03-25 17:24:32 +0800

    ..

--- src/Inventory/view/EditItem.java
+++ src/Inventory/view/EditItem.java
@@ -7,10 +7,6 @@ package Inventory.view;
 import dao.BaseDAO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
